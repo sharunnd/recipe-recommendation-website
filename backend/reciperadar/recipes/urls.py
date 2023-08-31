d4fbe5031aef741e5309f6e4e3b050b11a1a8ad5
Your Name <sharunnd11@gmail.com>
2023-08-31 18:13:38 +0530

    api endpoint changed

--- backend/reciperadar/recipes/urls.py
+++ backend/reciperadar/recipes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('api/get_recipe/', views.get_recipe_suggestions, name='get_recipe_suggestions'),
     path('api/create/', views.create_recipe, name='create-recipe'),
-    path('api/all_reipes', views.get_all_recipes, name='get-all-recipes'),
+    path('api/all_recipes', views.get_all_recipes, name='get-all-recipes'),
     path('api/update_recipe/<int:pk>/', views.update_recipe, name='update-recipe'),
     path('api/delete_recipe/<int:pk>/', views.delete_recipe, name='delete-recipe'),
 ]
