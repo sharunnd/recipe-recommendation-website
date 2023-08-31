27e307a3ef32759799890d297a4de6528f7df090
Your Name <sharunnd11@gmail.com>
2023-08-31 12:47:20 +0530

    creating recipe functionality done

--- backend/reciperadar/recipes/models.py
+++ backend/reciperadar/recipes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# recipes/models.py
+# recipes/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=255)
+    ingredients = models.TextField()
+
+    def __str__(self):
+        return self.heading
