a8d875079fe78471923ced11c6c09771a4824469
Your Name <sharunnd11@gmail.com>
2023-09-01 00:32:18 +0530

    single recipe fetch added

--- backend/reciperadar/recipes/urls.py
+++ backend/reciperadar/recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/all_recipes', views.get_all_recipes, name='get-all-recipes'),
     path('api/update_recipe/<int:pk>/', views.update_recipe, name='update-recipe'),
     path('api/delete_recipe/<int:pk>/', views.delete_recipe, name='delete-recipe'),
+    path('api/recipe/<int:recipe_id>/', views.get_recipe_by_id, name='get-recipe-by-id'),
 ]
